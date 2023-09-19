584b59797255a70efef20266a043744ca5ee64a7
Joseph Kwame Agyei <jagyei339@gmail.com>
2023-09-19 10:11:46 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void existance(){
+
+};
+
+void gmail_checks(){
+
+};
+
+void pass_req(){
+
+};
+
+void pass_match(){
+
+}
+
+int main(){
+    string options;
+    cout<<"Login / SignUp"<<endl;
+    getline(cin,options);
+    if(options=="Login" || options=="Logins"){
+        
+    } else{
+        
+    }
+
+return 0;
+};
