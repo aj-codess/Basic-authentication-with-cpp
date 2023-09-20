a00aa04ce0b9544751e38228f6e8e5d4fd63fb12
Joseph Kwame Agyei <jagyei339@gmail.com>
2023-09-20 11:44:27 +0000

    refactored

--- main.cpp
+++ main.cpp
@@ -1,41 +1,90 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
+
 vector<string>gmail_pool;
+vector<string>pass_pool;
 
-void password_dataBase(){
 
-}
+
+
 
 void existance(){
-    gmail_pool.push_back("jagyei339@gmail.com");
+    
     string temp;
     cout<<"Enter gmail"<<endl;
     getline(cin,temp);
+
+    string pass_temp;
+    cout<<"Enter Password"<<endl;
+    getline(cin,pass_temp);
+
     vector<string>::size_type num=0;
     while(num<gmail_pool.size()){
         if(temp==gmail_pool[num]){
-            cout<<"Access granted"<<endl;
+            vector<string>::size_type num2=0;
+            while(num2<pass_pool.size()){
+                if(pass_temp==pass_pool[num2]){
+                    cout<<"Data Secured in DataBase"<<endl
+                        <<"Access Granted"<<endl;
+                }
+                num2++;
+            }
         } else{
-            cout<<"access denied"<<endl;
+            cout<<"your initials doesnt exist"<<endl<<"Try Loging in Again"<<endl;
         }
         num++;
     };
 };
 
+
+
+
+
+void pass_checks(){
+
+    char symbols[] = {'!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '[', ']', '{', '}', '<', '>', '?'};
+
+    string temp_pass_hold;
+    cout<<"Enter a valid password to create"<<endl;
+    getline(cin,temp_pass_hold);
+    
+
+};
+
+
+
+
+
 void gmail_checks(){
+    string temp_hold;
+    cout<<"Enter gmail";
+    getline(cin,temp_hold);
 
+    string const find_string="@gmail.com";
+    string const find_second_string="@yahoo.com";
+
+    if(temp_hold.find(find_string) != string::npos || temp_hold.find(find_second_string) != string::npos){
+        pass_checks();
+    } else{
+        cout<<"Input was not a gmail"<<endl;
+        gmail_checks();
+    }
 };
 
-void pass_req(){
 
-};
+
+
+
 
 void pass_match(){
 
 }
+
+
 
 int main(){
     string options;
@@ -44,7 +93,7 @@ int main(){
     if(options=="Login" || options=="login"){
         existance();
     } else{
-        
+        gmail_checks();
     }
 
 return 0;
