87eabdbdcbc5b7dffcac9575fd83339b0cc30c3e
Joseph Kwame Agyei <jagyei339@gmail.com>
2023-09-22 12:55:50 +0000

    changes

--- main.cpp
+++ main.cpp
@@ -1,143 +1,89 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cstddef>
+#include <array>
 
 using namespace std;
 
-vector<string>gmail_pool;
-vector<string>pass_pool;
+vector<string> gmail_pool;
+vector<string> pass_pool;
 
+// void pass_match(){
 
+// };
 
+// Function to check if a password meets criteria and add it to the password pool
+void pass_checks(string temp_pass_hold) {
+    array<string,16> symbols = {"!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "[", "]", "{", "}", "<", ">", "?"};
+    array<string,10> numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
 
+    size_t get_symbol_length=size(symbols);
+    size_t get_numbers_length=size(numbers);
 
-void existance(){
-    
-    string temp;
-    cout<<"Enter gmail"<<endl;
-    getline(cin,temp);
+    bool symbol_bool = false;
+    bool digit_bool = false;
 
-    string pass_temp;
-    cout<<"Enter Password"<<endl;
-    getline(cin,pass_temp);
-
-    vector<string>::size_type num=0;
-    while(num<gmail_pool.size()){
-        if(temp==gmail_pool[num]){
-            vector<string>::size_type num2=0;
-            while(num2<pass_pool.size()){
-                if(pass_temp==pass_pool[num2]){
-                    cout<<"Data Secured in DataBase"<<endl
-                        <<"Access Granted"<<endl;
-                }
-                num2++;
-            }
-        } else{
-            cout<<"your initials doesnt exist"<<endl<<"Try Loging in Again"<<endl;
-        }
-        num++;
-    };
-};
-
-
-
-void pass_match(gen){
-   string first_pass=gen;
-   string second_pass;
-   cout<<"Enter Password Again"<<endl;
-   getline(cin,second_pass);
-   
-   if(first_pass===second_pass){
-        pass_pool.push_back(second_pass);
-   } else{
-    pass_checks();
-   };
-}
-
-
-
-
-void pass_checks(){
-
-    string symbols [] = {"!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "[", "]", "{", "}", "<", ">", "?"};
-    string numbers [] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
-
-    string temp_pass_hold;
-    cout<<"Enter a valid password to create"<<endl
-        <<"At Least password should have symbols and numbers within"<<endl;
-    getline(cin,temp_pass_hold);
-
-    
-    bool symbol_bool;
-    bool digit_bool;
-
-
-    if (temp_pass_hold.size() >= 8) {
-        int i = 0;
-        while (i < temp_pass_hold.size()) {
-            int inner_i = 0;
-            while (inner_i < sizeof(symbols) / sizeof(symbols[0])) {
-                if (temp_pass_hold[i] === symbols[inner_i]) {
+    if (temp_pass_hold.length() >= 8) {
+        for (int i=0;i<temp_pass_hold.length();i++) {
+            for (int sub_1=0;sub_1<get_symbol_length;sub_1++) {
+                if (temp_pass_hold[0] == symbols[sub_1]) {
                     symbol_bool = true;
-                    break; // Symbol found, no need to continue checking
+                    break; // Symbol found
                 };
-                inner_i++;
             };
-            
-            int sub_i = 0;
-            while (sub_i < sizeof(numbers) / sizeof(numbers[0])) {
-                if (temp_pass_hold[i] ==== numbers[sub_i]) {
+            for (int sub_2=0;sub_2<get_numbers_length;sub_2++) {
+                if (temp_pass_hold[i]==numbers[sub_2]) {
                     digit_bool = true;
-                    break; // Digit found, no need to continue checking
-                }
-                sub_i++;
+                    break; // Digit found
+                };
             };
-            
-            i++;
         };
     };
 
-
-    if(symbol_bool==true && digit_bool==true){
-        pass_match(temp_pass_hold);
+    if (symbol_bool && digit_bool) {
+        pass_pool.push_back(temp_pass_hold);
+        cout << "Password meets criteria......" << endl<<"Comparing....."<<endl;
+        // pass_match(temp_pass_hold);
+    } else {
+        cout << "Password does not meet criteria. Try again." << endl;
     };
-
-
 };
 
-
-
-
-void gmail_checks(){
+// Function to check if a Gmail address is valid and add it to the Gmail pool
+void gmail_checks() {
     string temp_hold;
-    cout<<"Enter gmail";
-    getline(cin,temp_hold);
+    cout << "Enter Gmail address: ";
+    getline(cin, temp_hold);
 
-    string const find_string="@gmail.com";
-    string const find_second_string="@yahoo.com";
+    string const find_string = "@gmail.com";
+    string const find_second_string = "@yahoo.com";
 
-    if(temp_hold.find(find_string) != string::npos || temp_hold.find(find_second_string) != string::npos){
+    if (temp_hold.find(find_string) != string::npos || temp_hold.find(find_second_string) != string::npos) {
         gmail_pool.push_back(temp_hold);
-        pass_checks();
-    } else{
-        cout<<"Input was not a gmail"<<endl;
+        string temp_pass_hold;
+        cout << "Enter a valid password to create an account: ";
+        getline(cin, temp_pass_hold);
+        pass_checks(temp_pass_hold);
+    } else {
+        cout << "Input was not a Gmail or Yahoo address." << endl;
         gmail_checks();
     }
-};
+}
 
-
-
-
-
-int main(){
+int main() {
     string options;
-    cout<<"Login / SignUp"<<endl;
-    getline(cin,options);
-    if(options=="Login" || options=="login"){
-        existance();
-    } else{
+    cout << "Login / SignUp" << endl;
+    getline(cin, options);
+
+    if (options == "Login" || options == "login") {
+        // Implement your login logic here
+        cout << "Not implemented: Login logic" << endl;
+    } else if (options == "SignUp" || options == "signup") {
         gmail_checks();
+    } else {
+        cout << "Invalid option." << endl;
     }
 
-return 0;
+    return 0;
 };
